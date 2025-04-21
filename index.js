6186f20e698bfd4a5d98b742ccac0e1d3a61acae
pq-Aahan <pulast.aahan@gmail.com>
2025-04-21 23:31:43 +0530

    commit

--- index.js
+++ index.js
@@ -3,9 +3,11 @@ const bodyParser = require('body-parser');
 const cors = require('cors');
 const connectDB = require('./src/config/database');
 const registerRoute = require('./src/routes/login'); // Adjust path
+require("dotenv").config(); // Load .env variables
 
 const app = express();
-const PORT = 5001;
+//const PORT = 5001;
+const PORT = process.env.PORT || 5001;
 
 // Middleware
 app.use(cors());
@@ -21,6 +23,11 @@ app.use('/api', registerRoute);
 connectDB();
 
 // Start Server
+// app.listen(PORT, () => {
+//     console.log(`Server running at http://localhost:${PORT}`);
+// });
 app.listen(PORT, () => {
     console.log(`Server running at http://localhost:${PORT}`);
 });
+module.exports = app;
+
--- src/config/database.js
+++ src/config/database.js
@@ -15,3 +15,28 @@ const connectDB = async () => {
 };
 
 module.exports = connectDB;
+// const mongoose = require('mongoose');
+
+// let isConnected = false;
+
+// const connectDB = async () => {
+//   if (isConnected) {
+//     console.log('=> Using existing database connection');
+//     return;
+//   }
+
+//   try {
+//     const conn = await mongoose.connect(process.env.MONGO_URI, {
+//       useNewUrlParser: true,
+//       useUnifiedTopology: true,
+//     });
+
+//     isConnected = true;
+//     console.log(`=> Connected to MongoDB: ${conn.connection.host}`);
+//   } catch (err) {
+//     console.error('MongoDB connection error:', err);
+//     throw err;
+//   }
+// };
+
+// module.exports = connectDB;
